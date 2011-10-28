1ec7c3195b4dd9b16d632475546bae84aab8448e
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 10:03:46 +0100

    top level search controller that extends the application controller (which should extend the cms controller) with options to paginate, change classes etc

---
+++ lib/controller/WildfireSearchController.php
@@ -0,0 +1,52 @@
+<?php
+
+class WildfireSearchController extends ApplicationController {
+
+  public $results = array();
+  public $search_fields = array('text'=>array('cols'=>array('title', 'content'), 'fuzzy'=>true));
+  public $paginate = false;
+  /**
+   * $model_filters = array(
+   *                        array('col'=>"column_name", 'val'=>"value", 'operator'=>"= / != / etc")
+   *                      );
+   */
+  public $model_filters = array();
+
+
+  public function index(){
+    //based on the cms content type & scope
+    $model = new $this->cms_content_class($this->cms_live_scope);
+    //grab the search param data
+    if($this->searched = Request::param('search')){
+      $sql = "";
+      //searh_fields key is the key in the post data - $_POST['search']['x']
+      foreach($this->search_fields as $post_key=>$config){
+        //if there is a matching var in the post data, we'll use it
+        if($term = $this->searched[$post_key]){
+          foreach($config['cols'] as $col){
+            //need to make these escaped, use pdo etc
+            if($config['fuzzy']) $sql .= "(`".$col."` LIKE '%".$term."%') OR";
+            else $sql .= "(`".$col."` = '".$term."') OR";
+          }
+        }
+      }
+      //apply the sql
+      if(strlen($sql)) $model->filter("(".rtrim($sql, " OR").")");
+      else $model->filter("1=0");
+      //if theres model filters set, apply them
+      if($this->model_filters) foreach($this->model_filters as $filter) $model->filter($filter['col'], $filter['val'], $filter['operator']);
+      //option to paginate or not
+      if($this->paginate){
+        if(!$this->this_page = Request::param('page')) $this->this_page = 1;
+        if($per = Request::param('per_page')) $this->per_page = $per;
+        $this->results = $model->page($this->this_page, $this->per_page);
+      }
+      else $this->results = $model->all();
+    }
+    
+  }
+
+
+
+}
+?>
