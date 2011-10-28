b5875d16ee22d0e6c5aae52b0ece2093627199e8
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 13:37:50 +0100

    read me

--- resources/public/javascripts/core/inline_search.js
+++ resources/public/javascripts/core/inline_search.js
@@ -14,6 +14,7 @@ jQuery(document).ready(function(){
           obj.removeClass("loading");
         },
         fail:function(res){
+          form.find(".inline_search_results").html("");
           obj.removeClass("loading").addClass("failed");
         }
       });
